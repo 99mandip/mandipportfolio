024770008e327c33042c6b2ecc4133cc7b7d6296
unknown <karkimandip826@gmail.com>
2024-07-26 20:58:35 +0545

    experience page

--- portfolio/resume/views.py
+++ portfolio/resume/views.py
@@ -8,4 +8,13 @@ def about(request):
     return render (request, "about.html")
 
 def projects (request):
-    return render (request,"projects.html")
+    projects_show=[
+        { 
+            'title':'Personal Portfolio',
+            'path':'images/portfolio.png',
+        },
+    ]
+    return render (request,"projects.html",{"projects_show": projects_show})
+
+def experience(request):
+    return render (request, "experience.html")
