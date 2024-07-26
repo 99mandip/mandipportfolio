650c7bc76982c4520b36ca8f8af82f756d4b008a
unknown <karkimandip826@gmail.com>
2024-07-26 14:16:27 +0545

    still making webpage 2 pages finished

--- portfolio/resume/views.py
+++ portfolio/resume/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render (request,"home.html")
 
-def about (request):
+def about(request):
     return render (request, "about.html")
+
+def projects (request):
+    return render (request,"projects.html")
