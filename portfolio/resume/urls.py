3ae813c38afc87c0108163e88a5a009739580b0f
unknown <karkimandip826@gmail.com>
2024-07-27 10:01:33 +0545

    created webpage

--- portfolio/resume/urls.py
+++ portfolio/resume/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("",views.home,name="home"),
     path("about/",views.about,name="about"),
     path("projects/",views.projects,name="projects"),
-    path("experience/", views.experience, name="experience")
-]
+    path("experience/", views.experience, name="experience"),
+    path("certificate/", views.certificate, name="certificate"),
+    path("contact/", views.contact, name="contact"),
+    path("resume/", views.resume, name="resume")
+]   
--- portfolio/resume/views.py
+++ portfolio/resume/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.conf import settings 
+import os 
 # Create your views here.
 def home(request):
     return render (request,"home.html")
@@ -17,4 +19,24 @@ def projects (request):
     return render (request,"projects.html",{"projects_show": projects_show})
 
 def experience(request):
-    return render (request, "experience.html")
+    experience=[
+        {"company":"Writelion Tech",
+         "position":"Front-end Developer"}
+    ]
+    return render (request,"experience.html",{"experience":experience})
+
+def certificate (request):
+    return render (request, "certificate.html")
+
+def contact (request):
+    return render (request, "contact.html")
+
+def resume (request):
+    resume_path=os.path.join(settings.STATIC_ROOT, 'mypdf/resume.pdf')
+    if os.path.exists(resume_path):
+        with open(resume_path, 'rb') as resume_file:
+            response = HttpResponse(resume_file.read(), content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename="resume.pdf"'
+            return response
+    else:
+        return HttpResponse("Resume not found", status=404)
